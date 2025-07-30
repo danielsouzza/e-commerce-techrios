b142812ed6084a133ec3449af84806239b555cfe
danielsouzza <daniel.lima.souzza@gmail.com>
2025-07-30 11:08:26 -0300

    fix: title btn

--- src/pages/OptOut.vue
+++ src/pages/OptOut.vue
@@ -188,7 +188,7 @@ function onPhoneInput(event) {
           block
           class="tw-mt-6"
         >
-          Solicitar remoçõa
+          Solicitar exclusão
         </v-btn>
 
         <v-card-text class="text-center tw-mt-4">
